c4b6333bd808c35008c85b86a23ead2d8e180e11
lleonor <leahaprilleonor@gmail.com>
2022-03-16 15:07:11 -0500

    Add project files.

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context) {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        //[HttpGet]
+        //public IEnumerable<Product> GetProducts() {
+        //    return _context.Products.ToArray();
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {            
+            return  Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProduct(int id) {
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public virtual List<Product>? Products { get; set; }
+    }
+
+}
+
+ 
